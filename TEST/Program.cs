7097244e2e6815a868c78d872542d3ae227e7c45
unknown <rxtmark@gmail.com>
2023-04-18 18:26:30 +0300

    Commit

---
+++ TEST/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SenderApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+                //Generate a public and private keys
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                RSAParameters publicKey = rsa.ExportParameters(false);
+                RSAParameters privateKey = rsa.ExportParameters(true);
+
+                //Create a message to sign
+                byte[] message = Encoding.UTF8.GetBytes("Kazkoks message");
+
+                //Create a digital signature
+                RSACryptoServiceProvider rsaPrivate = new RSACryptoServiceProvider();
+                rsaPrivate.ImportParameters(privateKey);
+                byte[] signature = rsaPrivate.SignData(message, new SHA256CryptoServiceProvider());
+
+                
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                int port = 12345;
+
+
+                using (Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sender.Connect(new IPEndPoint(ipAddress, port));
+
+                    //Send public key
+                    byte[] publicKeyBytes = Encoding.UTF8.GetBytes(Convert.ToBase64String(publicKey.Modulus) + "|" + Convert.ToBase64String(publicKey.Exponent));
+                    byte[] publicKeyLengthBytes = BitConverter.GetBytes(publicKeyBytes.Length);
+                    sender.Send(publicKeyLengthBytes);
+                    sender.Send(publicKeyBytes);
+
+                    //Send message
+                    byte[] messageLengthBytes = BitConverter.GetBytes(message.Length);
+                    sender.Send(messageLengthBytes);
+                    sender.Send(message);
+
+                    //Send signature
+                    byte[] signatureLengthBytes = BitConverter.GetBytes(signature.Length);
+                    sender.Send(signatureLengthBytes);
+                    sender.Send(signature);
+                }
+
+                Console.WriteLine("Sent message: {0}", Encoding.ASCII.GetString(message));
+                Console.WriteLine("Sent signature: {0}", Convert.ToBase64String(signature));
+                
+            }
+        }
+        
+    }
---
+++ TEST2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ReceiverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Same address and port as senders
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 12345;
+
+            using (Socket receiver = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                //Wait for a message and signature to be received
+                receiver.Bind(new IPEndPoint(ipAddress, port));
+                receiver.Listen(1);
+
+                Console.WriteLine("Waiting for connection...");
+
+                using (Socket client = receiver.Accept())
+                {
+                    Console.WriteLine("Connected!");
+
+                    byte[] buffer = new byte[1024];
+
+                    byte[] message = null;
+                    byte[] signature = null;
+
+                    //Receive public key
+                    byte[] publicKeyLengthBytes = new byte[4];
+                    client.Receive(publicKeyLengthBytes);
+
+                    int publicKeyLength = BitConverter.ToInt32(publicKeyLengthBytes, 0);
+                    byte[] publicKeyBytes = new byte[publicKeyLength];
+
+                    int bytesReceived = client.Receive(publicKeyBytes, 0, publicKeyLength, SocketFlags.None);
+                    if (bytesReceived != publicKeyLength)
+                    {
+                        Console.WriteLine("Error receiving public key.");
+                        return;
+                    }
+
+                    //Receive message
+                    byte[] messageLengthBytes = new byte[4];
+                    client.Receive(messageLengthBytes);
+
+                    int messageLength = BitConverter.ToInt32(messageLengthBytes, 0);
+                    message = new byte[messageLength];
+
+                    bytesReceived = client.Receive(message, 0, messageLength, SocketFlags.None);
+                    if (bytesReceived != messageLength)
+                    {
+                        Console.WriteLine("Error receiving message.");
+                        return;
+                    }
+
+                    //Receive signature
+                    byte[] signatureLengthBytes = new byte[4];
+                    client.Receive(signatureLengthBytes);
+
+                    int signatureLength = BitConverter.ToInt32(signatureLengthBytes, 0);
+                    signature = new byte[signatureLength];
+
+                    bytesReceived = client.Receive(signature, 0, signatureLength, SocketFlags.None);
+                    if (bytesReceived != signatureLength)
+                    {
+                        Console.WriteLine("Error receiving signature.");
+                        return;
+                    }
+
+                    // Convert public key bytes to RSAParameters object
+                    string publicKeyString = Encoding.UTF8.GetString(publicKeyBytes);
+                    string[] publicKeyParts = publicKeyString.Split('|');
+                    RSAParameters publicKey = new RSAParameters();
+                    publicKey.Modulus = Convert.FromBase64String(publicKeyParts[0]);
+                    publicKey.Exponent = Convert.FromBase64String(publicKeyParts[1]);
+
+                    //Test verification
+                    //Test(message, signature);
+
+                    //Verify the digital signature using the received public key
+                    RSACryptoServiceProvider rsaPublic = new RSACryptoServiceProvider();
+                    rsaPublic.ImportParameters(publicKey);
+
+                    Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(message));
+                    Console.WriteLine("Received signature: {0}", Convert.ToBase64String(signature));
+
+                    bool isVerified = rsaPublic.VerifyData(message, CryptoConfig.MapNameToOID("SHA256"), signature);
+
+                    string verificationResult = isVerified ? "Digital signature is verified." : "Digital signature is not verified.";
+                    Console.WriteLine(verificationResult);
+
+                }
+            }
+        }
+        public static void Test(byte[] message, byte[] signature) {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            RSAParameters publicKey2 = rsa.ExportParameters(false);
+            rsa.ImportParameters(publicKey2);
+            bool isVerified = rsa.VerifyData(message, CryptoConfig.MapNameToOID("SHA256"), signature);
+            string verificationResult = isVerified ? "Digital signature is verified." : "Digital signature is not verified.";
+            Console.WriteLine(verificationResult);
+        }
+
+    }
+}
+
